559360ee9e145b1ea3991b3b1bbfafc2aa261a7d
D0mazo <dominykas.simanavicius@proton.me>
2024-05-28 12:54:09 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Quotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Quotes
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
